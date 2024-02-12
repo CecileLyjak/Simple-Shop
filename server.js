78f84996be50cafdec278a96bec5363dba588e28
CecileLyjak <cece12.cy@gmail.com>
2024-02-12 22:21:21 +0100

    added main page

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const path = require('path');
 const shopRoutes = require('./src/shop/routes');
+const exp = require('constants');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+app.use(express.static(path.join(__dirname, 'src')));
+
+
 app.get("/", (req, res) => {
-    res.send("Hello");
+    res.render("mainPage");
 });
 
-app.use('/api/v1/shop', shopRoutes);
+app.use('/shop', shopRoutes);
 
 app.listen(port, () => console.log('app listning on port 3000'));
